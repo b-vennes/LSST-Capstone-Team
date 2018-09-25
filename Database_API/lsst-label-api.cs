ff38b538aa1dc0ab266d1a95c2407621e08b9195
Branden <b.n.vennes@gmail.com>
2018-09-24 20:14:34 -0700

    Basic GET API call added

--- Database_API/lsst-label-api.cs
+++ Database_API/lsst-label-api.cs
@@ -1,5 +1,7 @@
 
 using System;
+using System.Data;
+using System.Data.SqlClient;
 using System.Text;
 using System.IO;
 using System.Threading.Tasks;
@@ -18,7 +20,7 @@ namespace Database_API
 {
     public static class lsst_label_api
     {
-        [FunctionName("lsst_label_api")]
+        [FunctionName("lsst-label-api")]
         public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post", "get", Route = null)]HttpRequest req, ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
@@ -29,9 +31,38 @@ namespace Database_API
             CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
             CloudBlobContainer blobContainer = blobClient.GetContainerReference("lsst-image-container");
 
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "lsst-label-server.database.windows.net";
+            builder.UserID = "lsst-label-admin";
+            builder.Password = "QtGxJ/;T[8qx";
+            builder.InitialCatalog = "LSST-Label-Database";
+
             if (req.Method.Equals("GET"))
             {
-                return (ActionResult) new OkObjectResult("Congrats! You've attempted a GET! The API is alive!");
+                String response = "";
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    connection.Open();
+
+                    StringBuilder sb = new StringBuilder();
+
+                    sb.Append("SELECT TOP 1 URL FROM dbo.IMAGE_DATA");
+                    String querySql = sb.ToString();
+
+                    using (SqlCommand command = new SqlCommand(querySql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                response += reader.GetString(0);
+                            }
+                        }
+                    }
+                }
+
+                return (ActionResult) new OkObjectResult(response);
             }
             else if (req.Method.Equals("POST"))
             {
