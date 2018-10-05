a911c9213bba5c4f6702b3671fa17d82df7c0ee9
Branden <b.n.vennes@gmail.com>
2018-10-04 22:56:18 -0700

    added new functionality to post_image function

--- Pipeline/script.py
+++ Pipeline/script.py
@@ -1,7 +1,24 @@
-
+import os
+import requests
 
 def create_image():
     return 0
 
-def push_images():
-    return 0
+def post_images(image_path):
+
+    # put image file into byte array
+    with open(image_path, "rb") as imageFile:
+        f = imageFile.read()
+        b = bytearray(f)
+
+    # make request
+    data = requests.post("http://localhost:7071/api/lsst-label-api", data = b)
+
+    # delete file after it has been sent
+    os.remove(image_path)
+
+    # check status code
+    if (data.status_code == 200):
+        return 1
+    else:
+        return 0
