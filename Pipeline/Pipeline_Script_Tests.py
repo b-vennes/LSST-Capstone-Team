4e1dae550fd634e1b129a165530030f16b89def3
Branden <b.n.vennes@gmail.com>
2018-10-04 20:08:22 -0700

    added some additional tests focused around sending pre-existing images

--- Pipeline/Pipeline_Script_Tests.py
+++ Pipeline/Pipeline_Script_Tests.py
@@ -1,5 +1,5 @@
+import requests
 import script
-import pytest
 import os
 
 def test_create_image():
@@ -13,4 +13,33 @@ def test_create_image():
 
 def test_push_images():
     script.push_images()
-    assert False
+assert False
+
+'''
+    Test Description:
+    Attempt to reach the API and GET a 200 (OK) response.
+    Tests that the API can be connected to and is alive.
+'''
+def test_api_get_response():
+    data = requests.get("http://localhost:7071/api/lsst-label-api")
+    assert(data.status_code == 200)
+
+'''
+    Test Description:
+    Attempt to send nothing to the API, the API should return an error.
+'''
+def test_api_post_without_image():
+    data = requests.post("http://localhost:7071/api/lsst-label-api")
+    assert(data.status_code != 200)
+
+'''
+    Test Description:
+    Send an actual image and expect a 200 response.
+'''
+def test_send_image_to_api():
+    with open("spooky.jpg", "rb") as imageFile:
+        f = imageFile.read()
+        b = bytearray(f)
+
+    data = requests.post("http://localhost:7071/api/lsst-label-api", data = b)
+    assert(data.status_code == 200)
