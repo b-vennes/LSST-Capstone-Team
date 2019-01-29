e6acbed31fd4ad01dcd675d44029c99c6796f7a6
maggieryan3 <ryanmar19@up.edu>
2019-01-28 17:06:20 -0800

    Helper functions, cnn function (not tested)
    Having issues with tensorflow so I haven't tested this code yet

--- Space_Classifier/ml.py
+++ Space_Classifier/ml.py
@@ -12,6 +12,20 @@ import sys
 sys.path.append("..")
 from Pipeline.Database_Connect import DynamoConnect
 
+#General
+num_channels = 1 #for grayscale
+
+# Convolutional Layer 1.
+filter_size1 = 5          # Convolution filters are 5 x 5 pixels.
+num_filters1 = 16         # There are 16 of these filters.
+
+# Convolutional Layer 2.
+filter_size2 = 5          # Convolution filters are 5 x 5 pixels.
+num_filters2 = 36         # There are 36 of these filters.
+
+# Fully-connected layer.
+fc_size = 128             # Number of neurons in fully-connected layer.
+
 def build_and_train_cnn(image_arrays, image_labels, image_height, image_width, image_channels):
     # This site offers some loose guidance: https://www.tensorflow.org/tutorials/estimators/cnn
 
@@ -65,6 +79,96 @@ def build_and_train_cnn(image_arrays, image_labels, image_height, image_width, i
     # for now just return the results I guess
     return output
 
+def cnn(image_arrays, image_labels, image_height, image_width, image_channels):
+    # need to restructure image data into a format that tensorflow expects (have to add an inner channel because its grayscale)
+    image_arrays = numpy.expand_dims(image_arrays, axis=3)
+
+    # placeholder variable for our images array
+    input_placeholder = tf.placeholder(tf.float32, shape=(None, image_height, image_width, image_channels))
+
+    # first convolution layer - not sure about these values
+    convolution_layer_1, weights1 = new_conv_layer(input_placeholder, num_channels, filter_size1, num_filters1, True)
+
+    # second convolution layer
+    convolution_layer_2, weights2 = new_conv_layer(convolution_layer_1, num_channels, filter_size2, num_filters2, True)
+    
+    # flatten
+    layer_flat, num_features = flatten_layer(convolution_layer_2)
+
+    # fully connected layer 1
+    layer_fully1 = new_fullyconnected(layer_flat,1764,128,use_relu=True)
+
+    # fully connected layer 2
+    layer_fully2 = new_fullyconnected(layer_fully1,128,1,use_relu=False)
+
+    # predict
+    y_pred = tf.nn.softmax(layer_fully2)
+    
+    # return prediction
+    return y_pred
+
+# Creates new TensorFlow weights in the given shape 
+# and initializing them to random values 
+def new_weights(shape):
+    return tf.Variable(tf.truncated_normal(shape, stddev=0.05))
+	
+# Creates new TensorFlow biases in the given shape 
+# and initializing them to random values
+def new_biases(length):
+    return tf.Variable(tf.constant(0.05, shape=[length]))
+
+#Creates a new convolutional layer in the computational graph 
+def new_conv_layer(input, # previous layer
+                   num_input_channels, # number of channels in input
+                   filter_size, # width and height of each filter
+                   num_filters, # number of filters
+                   use_pooling=True): # 2x2 max-pooling
+
+    # shape of the filter-weights for the convolution
+    shape = [filter_size, filter_size, num_input_channels, num_filters]
+
+    # create new weights (filters with the given shape)
+    weights = new_weights(shape=shape)
+
+    # create new biases, one for each filter
+    biases = new_biases(length=num_filters)
+
+    # create the TensorFlow operation for convolution
+    layer = tf.nn.conv2d(input=input,filter=weights,strides=[1, 2, 2, 1],padding='SAME')
+
+    # add the biases to the results of the convolution
+    layer += biases
+
+    # use pooling to down-sample the image resolution
+    if use_pooling:
+        layer = tf.nn.max_pool(value=layer,ksize=[1, 2, 2, 1],strides=[1, 2, 2, 1],padding='SAME')
+
+    # rectified Linear Unit (ReLU) - calculates max(x, 0) for each input pixel x
+    layer = tf.nn.relu(layer)
+
+    # return both the resulting layer and the filter-weights
+    return layer, weights
+
+# Creates a new fully connected layer in the computational graph
+def flatten_layer(layer):
+    layer_shape = layer.get_shape()
+    num_features = layer_shape[1:4].num_elements()
+    
+    # reshape the layer to [num_images, num_features]
+    layer_flat = tf.reshape(layer, [-1, num_features])
+
+    # return both the flattened layer and the number of features.
+    return layer_flat, num_features
+
+def new_fullyconnected(input, num_inputs, num_outputs,use_relu=True):
+    weights = new_weights(shape=[num_inputs, num_outputs])
+    biases = new_biases(length=num_outputs)
+    layer = tf.matmul(input, weights) + biases
+    # Use ReLU?
+    if use_relu:
+        layer = tf.nn.relu(layer)
+
+    return layer
 
 def import_data():
     """
